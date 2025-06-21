2898c3f8345a54ce5eef1535bd580b44e22e820c
Woojin CHO <littletree0330@gmail.com>
2025-06-21 20:10:41 +0900

    chore: initialize MyBatis practice repository [ mybatis practice2 ]

---
+++ memberproject/src/test/java/com/mybatis/memberproject/MemberprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybatis.memberproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mybatis_join/src/test/java/com/kh/mybatis_join/MybatisJoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.mybatis_join;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisJoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
