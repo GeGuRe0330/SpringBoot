1337124c3a47c552eef6552b074c59bf0619a122
Woojin CHO <littletree0330@gmail.com>
2025-06-24 23:36:11 +0900

    chore: initialize MyBatis practice repository [ mybatis practice(file_upload ]

---
+++ memberproject_fileupload/src/main/java/com/practice/memberproject_fileupload/MemberprojectFileuploadApplication.java
@@ -0,0 +1,13 @@
+package com.practice.memberproject_fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberprojectFileuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberprojectFileuploadApplication.class, args);
+	}
+
+}
---
+++ memberproject_fileupload/src/test/java/com/practice/memberproject_fileupload/MemberprojectFileuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.memberproject_fileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberprojectFileuploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
